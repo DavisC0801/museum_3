8dcdf0737e719eb7338393ca66b371971a503cfc
davisc0801 <davisc1014@gmail.com>
2019-04-03 14:14:31 -0600

    Add tests for exhibit class.

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,4 +3,20 @@ require "./lib/exhibit"
 
 class ExhibitTest < Minitest::Test
 
+  def setup
+    @exhibit = Exhibit.new("Gems and Minerals", 0)
+  end
+
+  def test_it_exists
+    assert_isntance_of Exhibit, @exhibit
+  end
+
+  def test_it_has_a_name
+    assert_equal @exhibit.name, "Gems and Minerals"
+  end
+
+  def test_it_has_a_cost
+    assert_equal @exhibit.cost, 0
+  end
+
 end
